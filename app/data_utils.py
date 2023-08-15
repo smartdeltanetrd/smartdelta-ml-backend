f4df715e27addf669992d74bd40a06890f3854ae
Yury Yakhno <yury.yakhno@mera.com>
2023-08-15 15:19:41 +0300

    Add container for srver, restore logic for dev container

--- app/data_utils.py
+++ app/data_utils.py
@@ -2,7 +2,6 @@ from cgi import FieldStorage
 from flask import Blueprint
 from typing import Optional, Tuple
 from app.constants import CSV_EXTENSION, TRAIN_SIMULATION_DELAY
-import pandas as pd
 import time
 import logging
 
@@ -12,8 +11,8 @@ bp = Blueprint('data_utils', __name__)
 logging.basicConfig(level=logging.INFO)
 
 def read_csv_to_dataframe(file_obj):
-    df = pd.read_csv(file_obj)
-    return df
+    # df = pd.read_csv(file_obj)
+    return None #df
 
 
 def validate_and_get_file(request) -> Tuple[Optional[FieldStorage], Optional[str]]:
